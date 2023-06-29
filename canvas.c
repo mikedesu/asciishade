48df25c1902d3c7b8c94cca4e7307db6ce684c8d
darkmage <165251+mikedesu@users.noreply.github.com>
2023-06-28 21:25:28 -0500

    reformating braces

--- canvas.c
+++ canvas.c
@@ -2,46 +2,31 @@
 #include <stdio.h>
 #include "canvas.h"
 
-
-
-canvas_pixel_t **init_canvas(int height, int width) 
-{
+canvas_pixel_t **init_canvas(int height, int width) {
     canvas_pixel_t **canvas = NULL;
-
     // do some basic checks on the input parameters
-    if (width < 1 || height < 1) 
-    {
+    if (width < 1 || height < 1) {
         fprintf(stderr, "Error: width and height must be greater than 0\n");
         exit(EXIT_FAILURE);
     }
-
-    //canvas_width  = width;
-    //canvas_height = height;
     canvas = calloc(height, sizeof(canvas_pixel_t *));
-    for (int i = 0; i < height; i++) 
-    {
+    for (int i = 0; i < height; i++) {
         canvas[i] = calloc(width, sizeof(canvas_pixel_t));
-        for (int j = 0; j < width; j++) 
-        {
+        for (int j = 0; j < width; j++) {
             canvas[i][j].foreground_color = 0;
             canvas[i][j].background_color = 0;
             canvas[i][j].character = ' ';
         }
     }
-
     return canvas;
 }
 
 
-
-void free_canvas(canvas_pixel_t **canvas, int canvas_height)
-{
+void free_canvas(canvas_pixel_t **canvas, int canvas_height) {
     printf("Freeing the canvas memory...\n");
-    for (int i = 0; i < canvas_height; i++) 
-    {
+    for (int i = 0; i < canvas_height; i++) {
         free(canvas[i]);
     }
     free(canvas);
 }
-
 
--- canvas.h
+++ canvas.h
@@ -3,23 +3,12 @@
 #include <wchar.h>
 
 // this is the base of the canvas
-typedef struct 
-{
+typedef struct {
     wchar_t character;
     int foreground_color;
     int background_color;
 } canvas_pixel_t;
 
-
-typedef struct 
-{
-    int width;
-    int height;
-    canvas_pixel_t **canvas;
-} canvas_t;
-
-
 canvas_pixel_t **init_canvas(int width, int height);
 void free_canvas(canvas_pixel_t **canvas, int canvas_height);
-
 
